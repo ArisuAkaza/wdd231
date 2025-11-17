7ff6679f1b0fbc5729968385df949355efe44b36
ArisuAkaza <alec.j.nugent@gmail.com>
2025-11-17 11:37:07 -0700

    Chamber commit1

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,48 @@
+const members= document.querySelector('#members');
+const gridButton= document.querySelector('#grid');
+const listButton= document.querySelector('#list');
+
+async function getMembersData() {
+    const response = await fetch('data/members.json');
+    const data = await response.json();
+    console.table(data.businesses);
+    displayMembers(data.businesses);
+}
+getMembersData();
+
+const displayMembers = (membersData) => {
+    membersData.forEach((business) => {
+        // some code
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let logo = document.createElement('img');
+
+        name.textContent = business.name;
+        address.textContent = `Address: ${business.address}`;
+        phone.textContent = `Phone: ${business.phone}`;
+        website.textContent = business.website;
+        website.href = business.website;
+        website.target = "_blank";
+        logo.src = business.logo;
+        logo.alt = `Logo of ${business.name}`;
+        logo.loading = "lazy";
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        members.appendChild(card);
+    });
+}
+
+gridButton.addEventListener('click', () => {
+    members.classList.add('grid');
+});
+listButton.addEventListener('click', () => {
+    members.classList.remove('grid');
+});
